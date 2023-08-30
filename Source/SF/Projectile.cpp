b0c141d44429c09920da135ed9cb514fe246087c
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-08-30 15:09:56 +0100

    Projectile tweaking

--- Source/SF/Projectile.cpp
+++ Source/SF/Projectile.cpp
@@ -54,7 +54,6 @@ void AProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimi
 void AProjectile::OnOverlapStart(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
 	if(OtherActor != UGameplayStatics::GetPlayerPawn(GetWorld(), 0)){
-	UE_LOG(LogTemp, Error, TEXT("Laser Overlap %s"), *OtherActor->GetActorNameOrLabel());
 	Destroy();}
 }
 
