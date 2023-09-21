e5c2ec43105dece1acdb1cd2387139610832efc9
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-09-21 10:00:49 +0100

    Ground Enemy init

--- Source/SF/LandEnemyPawn.cpp
+++ Source/SF/LandEnemyPawn.cpp
@@ -2,4 +2,54 @@
 
 
 #include "LandEnemyPawn.h"
+#include "GunComponent.h"
+#include "Components/CapsuleComponent.h"
+
+
+ALandEnemyPawn::ALandEnemyPawn()
+{
+    PrimaryActorTick.bCanEverTick = true;
+    CapsuleComponent = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Capsule Comp"));
+    SetRootComponent(CapsuleComponent);
+    MainBodyComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MainBody"));
+    MainBodyComponent->SetupAttachment(CapsuleComponent);
+    GunComponent = CreateDefaultSubobject<UGunComponent>(TEXT("Gun"));
+    GunComponent->SetupAttachment(MainBodyComponent);
+    TurretMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
+    TurretMeshComponent->SetupAttachment(GunComponent);
+    SingleLaserSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("LaserSpawnPoint"));
+    SingleLaserSpawnPoint->SetupAttachment(TurretMeshComponent);
+    DoubleLaserSpawnPointL = CreateDefaultSubobject<USceneComponent>(TEXT("LaserSpawnPointL"));
+    DoubleLaserSpawnPointL->SetupAttachment(TurretMeshComponent);
+    LaserSpawnPoints.Add(DoubleLaserSpawnPointL);
+    DoubleLaserSpawnPointR = CreateDefaultSubobject<USceneComponent>(TEXT("LaserSpawnPointR"));
+    DoubleLaserSpawnPointR->SetupAttachment(TurretMeshComponent);
+    LaserSpawnPoints.Add(DoubleLaserSpawnPointR);
+    
+}
+
+void ALandEnemyPawn::BeginPlay()
+{
+    Super::BeginPlay();
+    if(GunComponent!=nullptr){
+        GunComponent->SetupGunComponent(this, Speed, -1, false, SingleLaserSpawnPoint, LaserSpawnPoints);
+    }
+}
+void ALandEnemyPawn::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+void ALandEnemyPawn::Move()
+{
+    
+}
+
+void ALandEnemyPawn::SetRotation()
+{
+    if(MovementNodes.Num()>0){
+        
+    }
+}
+
 
--- Source/SF/LandEnemyPawn.h
+++ Source/SF/LandEnemyPawn.h
@@ -6,13 +6,55 @@
 #include "MovingPawn.h"
 #include "LandEnemyPawn.generated.h"
 
+class UGunComponent;
+class UCapsuleComponent;
 /**
- * 
+ *
  */
 UCLASS()
 class SF_API ALandEnemyPawn : public AMovingPawn
 {
 	GENERATED_BODY()
+
+public:
+	ALandEnemyPawn();
+
 protected:
+	virtual void BeginPlay() override;
+	virtual void Move() override;
+	virtual void SetRotation() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+private:
 	
+	UPROPERTY(EditAnywhere)
+	UCapsuleComponent *CapsuleComponent;
+
+	UPROPERTY(EditAnywhere)
+	USceneComponent* SingleLaserSpawnPoint;
+	UPROPERTY(EditAnywhere)
+	USceneComponent* DoubleLaserSpawnPointL;
+	UPROPERTY(EditAnywhere)
+	USceneComponent* DoubleLaserSpawnPointR;
+	TArray<USceneComponent*> LaserSpawnPoints;
+
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* MainBodyComponent;
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* TurretMeshComponent;
+
+	UPROPERTY(EditAnywhere)
+	UGunComponent *GunComponent;
+
+	UPROPERTY(EditAnywhere)
+	TArray<AActor *> MovementNodes;
+
+	AActor* CurrentTarget;
+	int32 CurrentTargetIndex;
+	UPROPERTY(EditAnywhere)
+	bool bLoopsPositions = false;
+
+
 };
