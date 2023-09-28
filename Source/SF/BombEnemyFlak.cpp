13949e4c2e5afba5c909364c8b65a45d0c75f687
Joaquin <74403807+LordMrMittens@users.noreply.github.com>
2023-09-28 10:51:49 +0100

    Boss Flak Gun Working
    But will need tweaking

--- Source/SF/BombEnemyFlak.cpp
+++ Source/SF/BombEnemyFlak.cpp
@@ -2,4 +2,61 @@
 
 
 #include "BombEnemyFlak.h"
+#include "GameplayManager.h"
+#include "Kismet/GameplayStatics.h"
 
+ABombEnemyFlak::ABombEnemyFlak()
+{
+    
+}
+
+    
+
+
+void ABombEnemyFlak::BeginPlay()
+{
+    Super::BeginPlay();
+    GameplayManager = Cast<AGameplayManager>(UGameplayStatics::GetActorOfClass(GetWorld(),AGameplayManager::StaticClass()));
+}
+
+void ABombEnemyFlak::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+    float DistanceToPlayer = CalculateYZDistanceToPlayer();
+    float XDistanceToPlayer = CalculateXDistanceToPlayer();
+    if(DistanceToPlayer > PreviousDistanceToPlayer || DistanceToPlayer < MinDistanceToExplode)
+    {
+        bYZDistanceMet = true;
+    }
+    if (bYZDistanceMet && XDistanceToPlayer < MinXDistanceToExplode * 1.5f)
+    {
+        Explode();
+    }
+    if (XDistanceToPlayer < MinXDistanceToExplode)
+    {
+        Explode();
+    }
+    PreviousDistanceToPlayer = DistanceToPlayer;
+}
+
+float ABombEnemyFlak::CalculateYZDistanceToPlayer()
+{
+    if(GameplayManager != nullptr)
+    {
+        FVector MyYZLocation = FVector(GetActorLocation().X, 0, 0);
+        FVector PlayerYZLocation = FVector(GameplayManager->GetPlayerLocation().X, 0, 0);
+        return FVector::Dist(MyYZLocation, PlayerYZLocation);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Display, TEXT("GameplayManager is null"));
+        return 0;
+    }
+}
+
+float ABombEnemyFlak::CalculateXDistanceToPlayer()
+{
+    FVector MyXLocation = FVector(0, GetActorLocation().Y, GetActorLocation().Z);
+    FVector PlayerXLocation = FVector(0, GameplayManager->GetPlayerLocation().Y, GameplayManager->GetPlayerLocation().Z);
+    return FVector::Dist(MyXLocation, PlayerXLocation);
+}
--- Source/SF/BombEnemyFlak.h
+++ Source/SF/BombEnemyFlak.h
@@ -6,6 +6,7 @@
 #include "Bomb.h"
 #include "BombEnemyFlak.generated.h"
 
+class AGameplayManager;
 /**
  * 
  */
@@ -14,4 +15,22 @@ class SF_API ABombEnemyFlak : public ABomb
 {
 	GENERATED_BODY()
 	
+	protected:
+	void BeginPlay() override;
+	public:
+	ABombEnemyFlak();
+	void Tick(float DeltaTime) override;
+
+	private:
+	AGameplayManager* GameplayManager;
+	float CalculateYZDistanceToPlayer();
+	float CalculateXDistanceToPlayer();
+	float PreviousDistanceToPlayer = 9999999999;
+	UPROPERTY(EditAnywhere, Category = "ExplosionLimits")
+	float MinDistanceToExplode =50;
+		UPROPERTY(EditAnywhere, Category = "ExplosionLimits")
+	float MinXDistanceToExplode =1000;
+
+	bool bYZDistanceMet = false;
+
 };
